6594151793875786f4aab7da8e4e63d69c9e24ed
Swapna Panchomarthi <swapnapanchomarthi@zipcoders-MacBook-Pro.local>
2019-02-13 14:39:44 -0500

    Swapna Person TDD

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -1,35 +1,106 @@
 package com.zipcodewilmington.person;
 
+import java.util.Date;
+
 /**
  * Created by leon on 2/12/18.
  */
 public class Person {
     private String name;
     private int age;
+    private String dept;
+    private String jobRole;
+    private String company;
+    private double salary;
+    private int jobId;
 
     public Person() {
+       this.name="";
+       this.age=Integer.MAX_VALUE;
+       this.dept="";
+       this.company="";
+       this.jobId=Integer.MAX_VALUE;
+       this.salary=Double.MIN_VALUE;
+       this.jobRole="";
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name=name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person (String jobRole, double sal){
+        this.jobRole=jobRole;
+        this.salary=sal;
+    }
+    public void details(String company, String jRole)
+    {
+        this.company=company;
+        this.jobRole=jRole;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age=age;
     }
 
     public String getName() {
-        return null;
+
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+    }
+    public String getDept( ) {
+
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getJobRole() {
+        return jobRole;
+    }
+
+    public void setJobRole(String jobRole) {
+        this.jobRole = jobRole;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(int jobId) {
+        this.jobId = jobId;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -3,15 +3,41 @@ package com.zipcodewilmington.person;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Date;
+
 /**
  * Created by leon on 2/12/18.
  */
 public class TestPerson {
     @Test
-    public void testDefaultConstructor() {
+    public void testDefaultConstructor1() {
         // Given
         String expectedName = "";
         Integer expectedAge = Integer.MAX_VALUE;
+        double salary = Double.MIN_VALUE;
+        int jobId = Integer.MAX_VALUE;
+
+        // When
+        Person person = new Person();
+
+        // Then
+        String actualName = person.getName();
+        Integer actualAge = person.getAge();
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedAge, actualAge);
+    }
+
+    @Test
+    public void testDefaultConstructor2() {
+        // Given
+        String expectedName = "";
+        Integer expectedAge = Integer.MAX_VALUE;
+        String expecteddept = "";
+        String jobRole = "";
+        String company = "";
+        double salary = Double.MIN_VALUE;
+        int jobId = Integer.MAX_VALUE;
 
         // When
         Person person = new Person();
@@ -94,5 +120,37 @@ public class TestPerson {
         // Then
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetDept() {
+        //Given
+        Person person = new Person();
+        String expectedDept = "OPT";
+
+        //When
+        person.setDept(expectedDept);
+
+        //Then
+        String actual = person.getDept();
+        Assert.assertEquals(expectedDept, actual);
+    }
+
+    @Test
+    public void testConstructorWithJobRoleAndSalary(){
+        //Given
+
+            String expectedJobRole = "Developer";
+            double expectedSalary = 120.00;
+
+            //When
+        Person person = new Person(expectedJobRole,expectedSalary);
+
+        //Then
+        String actualRole = person.getJobRole();
+        double actuaSal = person.getSalary();
+        Assert.assertEquals(expectedJobRole, actualRole);
+        Assert.assertEquals(expectedSalary, actuaSal, 0.0);
+
     }
 }
